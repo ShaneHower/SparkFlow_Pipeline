7401c4e4af22e7e8ff5d0dc9df834496eac08977
Shane Hower <shane.hower@gmail.com>
2024-06-27 13:09:07 -0400

    Add Remote Development Capabilities (#7)
    * Consolidate python reqs into requirements file to optimize airflow Dockerfile
    
    * Add AWS requirements
    
    * Add an check file operator and helper py files
    
    * pare down requirements file
    
    * Add init files
    
    * Ignore pem files
    
    * Cloud development/testing added

---
+++ sparkflow/dags/core/config.py
@@ -0,0 +1,30 @@
+from datetime import timedelta
+
+class Config:
+    def __init__(self):
+        """This class is here for convenience, it stores variables that are shared throughout every DAG.  This is helpful
+        as we only have to change these DAG variables in one place if the need arises.
+
+        Attributes:
+            bucket_name: S3 bucket name
+            input_location: The location where we are monitoring input files
+            jar_location: We store the JARs on S3 and pass them into the spark cluster to execute.
+            spark_conn_id: The name of the Airflow connection ID for the spark cluster we are running on
+            airflow_default_args: Airflow default arguments for a basic DAG
+        """
+        # S3 info
+        self.bucket_name = 'picklepokeyhouse'
+        self.input_location = 'finances/monitored_input'
+        self.jar_location = 's3a://picklepokeyhouse/sparkflow_jars'
+
+
+        # Airflow info
+        self.spark_conn_id = 'spark_default'
+        self.airflow_default_args = {
+            'owner': 'airflow',
+            'depends_on_past': False,
+            'email_on_failure': False,
+            'email_on_retry': False,
+            'retries': 1,
+            'retry_delay': timedelta(minutes=5),
+        }
