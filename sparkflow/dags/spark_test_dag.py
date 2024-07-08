a900c67367b21dded62c28f0a0ca571294625d61
root <root@ip-172-31-54-171.ec2.internal>
2024-07-08 16:36:29 +0000

    Add code to run spark job if file exists

--- sparkflow/dags/spark_test_dag.py
+++ sparkflow/dags/spark_test_dag.py
@@ -8,40 +8,46 @@ from utils.helper_functions import get_half_system_resources
 
 # Airflow
 from airflow import DAG
-from airflow.operators.python import PythonOperator
+from airflow.operators.python import BranchPythonOperator, PythonOperator
+from airflow.operators.dummy import DummyOperator
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 
 
+FILE_TYPE = 'capital_one'
+
 def check_s3_file(config: Config):
-    key = f'{config.input_location}/capital_one'
-    return S3Hook().check_for_key(key, config.bucket_name)
+    prefix = f'{config.input_location}/input/{FILE_TYPE}/'
+    keys = S3Hook().list_keys(bucket_name=config.bucket_name, prefix=prefix)
 
-
-def submit_and_forget(config: Config, file_exists: bool):
-    if file_exists:
-        memory_alloc, cpu_alloc = get_half_system_resources()
-
-        try:
-            task = SparkSubmitOperator(
-                task_id='submit_spark_test',
-                application=f'{config.jar_location}/dag-manager_2.12-1.0.jar',
-                name='airflow-spark-test-job',
-                conn_id=config.spark_conn_id,
-                executor_cores=cpu_alloc,
-                executor_memory=f'{memory_alloc}g',
-                num_executors='1',
-                verbose=True,
-                status_poll_interval=0.5,
-                deploy_mode='cluster',
-                java_class='com.main.DAGManager'
-            )
-            task.execute(context={})
-        except Exception as e:
-            logging.info(f'Exception: {e}')
-
+    if keys:
+        logging.info(f'There are new {FILE_TYPE} files to process')
+        return 'submit_and_forget'
     else:
-        logging.info('There is no Capital One file in input location.  Nothing to process.')
+        logging.info(f'There are NO new {FILE_TYPE} files to process')
+        return 'file_does_not_exist'
+
+
+def submit_and_forget(config: Config):
+    memory_alloc, cpu_alloc = get_half_system_resources()
+
+    try:
+        task = SparkSubmitOperator(
+            task_id='submit_spark_test',
+            application=f'{config.jar_location}/dag-manager_2.12-1.0.jar',
+            name='airflow-spark-test-job',
+            conn_id=config.spark_conn_id,
+            executor_cores=cpu_alloc,
+            executor_memory=f'{memory_alloc}g',
+            num_executors='1',
+            verbose=True,
+            status_poll_interval=0.5,
+            deploy_mode='cluster',
+            java_class='com.main.DAGManager'
+        )
+        task.execute(context={})
+    except Exception as e:
+        logging.info(f'Exception: {e}')
 
 
 logging.info('Begin')
@@ -57,15 +63,15 @@ with DAG(
     catchup=False,
 ) as dag:
 
-    check_file_exists = PythonOperator(
-        task_id='check_s3_file_exists',
+    check_file_exists = BranchPythonOperator(
+        task_id='check_file_exists',
         python_callable=check_s3_file,
         op_kwargs={
             'config': config
         }
     )
 
-    submit_spark_job = PythonOperator(
+    submit_and_forget = PythonOperator(
         task_id='submit_and_forget',
         python_callable=submit_and_forget,
         op_kwargs={
@@ -73,6 +79,10 @@ with DAG(
         }
     )
 
+    file_does_not_exist = DummyOperator(
+        task_id='file_does_not_exist',
+    )
 
-    # Set task dependencies
-    check_file_exists >> submit_spark_job
+
+    # Set task dependencies.  We are using the BranchPythonOperator to determine if we should send the spark job or not.
+    check_file_exists >> [submit_and_forget, file_does_not_exist]
