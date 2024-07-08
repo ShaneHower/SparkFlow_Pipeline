82ea877ee0aa799155265bdbb81b30a366ce5cb1
EC2 Default User <ec2-user@ip-172-31-57-210.ec2.internal>
2024-07-08 20:00:18 +0000

    List files in scala dag

--- scala_dags/src/main/scala/Main.scala
+++ scala_dags/src/main/scala/Main.scala
@@ -1,15 +1,9 @@
 package com.main
 
 import org.apache.spark.sql.{DataFrame, SparkSession}
-import software.amazon.awssdk.auth.credentials.{ProfileCredentialsProvider, AwsCredentials}
+import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.hadoop.fs.s3a.S3AFileSystem
-
-
-// object Config {
-//     val credentials: AwsCredentials = ProfileCredentialsProvider.create().resolveCredentials()
-//     val accessKeyId: String = credentials.accessKeyId()
-//     val secretAccessKey: String = credentials.secretAccessKey()
-// }
+import scala.collection.mutable.ListBuffer
 
 
 object DAGManager {
@@ -18,19 +12,34 @@ object DAGManager {
         println("starting connection")
         val spark = SparkSession.builder()
             .appName("Read S3 Test")
-            // NOTE: These are used when locally developing we may want to add functionality of "local" vs "cloud" development
-            // .config("spark.hadoop.fs.s3a.access.key", Config.accessKeyId)
-            // .config("spark.hadoop.fs.s3a.secret.key", Config.secretAccessKey)
             .config("spark.hadoop.fs.s3a.impl", classOf[S3AFileSystem].getName)
             .config("spark.hadoop.fs.s3a.endpoint", "s3.amazonaws.com")
             .config("spark.hadoop.fs.s3a.path.style.access", "true")
             .getOrCreate()
         println("connection made")
 
-        val s3Path: String = "s3a://picklepokeyhouse/finances/monitored_input/capital_one/input/qucksilver_202401_to_202404.csv"
+        val s3Path: String = "s3a://picklepokeyhouse/finances/monitored_input/input/capital_one/"
 
-        println("reading file")
-        val df: DataFrame = spark.read.text(s3Path)
+        // List all files in the given S3 path
+        val fs = FileSystem.get(spark.sparkContext.hadoopConfiguration)
+        val path = new Path(s3Path)
+        val fileStatuses = fs.listStatus(path)
+        val filesList = ListBuffer[String]()
+
+        
+
+        for (status <- fileStatuses) {
+            if (status.isFile) {
+                filesList += status.getPath.toString
+            }
+        }
+
+        println("Files found:")
+        filesList.foreach(println)
+
+        // Read all files into a DataFrame
+        println("reading files")
+        val df: DataFrame = spark.read.text(filesList: _*)
 
         df.show()
 
