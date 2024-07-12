64de35522e3cba4f3c9f5c26623f4eab16df3a81
ShaneHower <shane.hower@gmail.com>
2024-07-12 12:24:44 -0400

    Update lambda using the git key for cloud development

--- infra/lambda/SparkFlowEC2Launcher/lambda_function.py
+++ infra/lambda/SparkFlowEC2Launcher/lambda_function.py
@@ -5,6 +5,7 @@ AMI = os.environ['AMI']
 INSTANCE_TYPE = os.environ['INSTANCE_TYPE']
 KEY_NAME = os.environ['KEY_NAME']
 REGION = os.environ['REGION']
+GIT_PASS_KEY = os.environ['GIT_PASS_KEY']
 
 ec2 = boto3.client('ec2', region_name=REGION)
 
@@ -23,7 +24,7 @@ def lambda_handler(event, context):
     with open('init_instructions.txt', 'r') as file:
         init_script = file.read()
 
-    print(init_script)
+    init_script = init_script.replace('{GIT_PASS_KEY}', GIT_PASS_KEY)
 
     instance = ec2.run_instances(
         ImageId=AMI,
